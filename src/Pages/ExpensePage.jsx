b5ff34a26a0788f9b399c639ba538469d247c775
sarthakgawri889 <144597233+sarthakgawri889@users.noreply.github.com>
2024-11-27 02:59:39 +0530

    d

--- src/Pages/ExpensePage.jsx
+++ src/Pages/ExpensePage.jsx
@@ -134,7 +134,7 @@ function ExpensePage() {
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
-          marginTop : '9vh'
+          marginTop : '8vh'
         }}
       >
         <Typography
