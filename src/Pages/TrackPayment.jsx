b74a9b0b6c4ed6c8f2776e59956015e4c02ec6ff
sarthakgawri889 <144597233+sarthakgawri889@users.noreply.github.com>
2024-11-27 03:27:34 +0530

    s

--- src/Pages/TrackPayment.jsx
+++ src/Pages/TrackPayment.jsx
@@ -112,7 +112,7 @@ const TrackPayment = () => {
                   <Typography variant="h6" color="primary">
                     Shop Name: {sale.shopName} | Date: {dayjs(sale.date).format("DD/MM/YYYY")}
                   </Typography>
-                  <TableContainer component={Paper} sx={{ mt: 2, height:'100vh' }}>
+                  <TableContainer component={Paper} sx={{ mt: 2 }}>
                     <Table size="small">
                       <TableHead>
                         <TableRow>
